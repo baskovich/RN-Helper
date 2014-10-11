eaf51cf14898e7496c2e9fba328b9c3e3f2c3ef3
Тимур <onfolcpd@gmail.com>
2014-10-11 20:48:14 +0400

    Рабочий прототип

--- js/background.js
+++ js/background.js
@@ -35,27 +35,63 @@ function showWindow (name) {
 }
 
 function onShowWindowInvite () {
-    rnRequest('HighscoreInterface', 'search', ['00000000-0000-0000-0000-000000000000', 9, 200], function(data){
+    rnRequest('HighscoreInterface', 'search', ['00000000-0000-0000-0000-000000000000', 9, 10000], function(data){
         if (typeof data.Body.ranking == 'undefined') {
             console.log(data);
             return;
         }
         var inviteGroups = $('#rn-invite-groups');
         for (var i in data.Body.ranking) {
-            var group = $('<div><input type="checkbox" data-id="' + data.Body.ranking[i].corporation_id + '">&nbsp;' + data.Body.ranking[i].nick + '</div>');
+            var groupId = data.Body.ranking[i].corporation_id;
+            var name = data.Body.ranking[i].nick;
+            var group = $('<div><input type="checkbox" data-id="' + groupId + '">&nbsp;' + name + '</div>');
             inviteGroups.append(group);
         }
         checkboxInvite();
     });
+
+    $('#rn-invite-submit').on('click', onInviteSubmit);
 }
 
 function checkboxInvite () {
     $('#rn-invite-groups input[type=checkbox]').on('change', function(){
+        var inviteUsers = $('#rn-invite-users');
+        var id = $(this).data('id');
         if ($(this).is(':checked')) {
-
+            rnRequest('CorporationInterface', 'get', [id], onCheckedGroup);
         } else {
-
+            $('input[type=checkbox][data-group-id=' + id + ']', inviteUsers).parent().remove();
         }
-        console.log($(this).data('id'));
     });
+}
+
+function onCheckedGroup (data) {
+    var inviteUsers = $('#rn-invite-users');
+    var groupId = data.Body.ID;
+    for (var i in data.Body.members) {
+        var userId = data.Body.members[i].user_id;
+        var name = data.Body.members[i].name;
+        var user = $('<div><input type="checkbox" data-group-id="' + groupId + '" data-user-id="' + userId + '" checked>&nbsp;' + name + '</div>');
+        inviteUsers.append(user);
+    }
+}
+
+function onInviteSubmit () {
+    var btn = $('#rn-invite-submit');
+    if (!btn.hasClass('inactive')) {
+        btn.text('Подождите...').addClass('inactive');
+        var checkboxes = $('#rn-invite-users input[type=checkbox]');
+        var count = checkboxes.length;
+        checkboxes.each(function(index, el){
+            if ($(el).is(':checked')) {
+                var id = $(el).data('user-id');
+                rnRequest('CorporationInterface', 'invite', [id]);
+            }
+            if (!--count) doInviteUsers();
+        });
+    }
+}
+
+function doInviteUsers () {
+    alert('Все пользователи приглашены');
 }
